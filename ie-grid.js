39d01c7c2d3d35e438810598971b616a07e50f9d
i-sayer <i-sayer@users.noreply.github.com>
2019-04-18 21:23:34 +1200

    add grid demo

---
+++ ie-grid.js
@@ -0,0 +1,20 @@
+/* fake modern grid on IE9,10,11 */
+(function(){
+    function calccardsize(e) {
+        var card, cards, i, ss = document.styleSheets[0],
+            domrect = document.querySelector(".agrid").getBoundingClientRect(),
+            windowwidth = domrect.width,
+            cols = Math.floor(windowwidth/320),
+            extra = (windowwidth-(cols*320)-((cols)*14))/cols,
+            cardwidth = Math.floor(320+extra);
+        cards = document.querySelectorAll(".agrid>.card");
+        for (i=0; card = cards[i]; i++)
+        card.style.width = cardwidth+"px";//card.style.flex = "0 0 "+cardwidth+"px";
+    }
+    var ss = document.styleSheets[0];
+    //ss.insertRule(".agrid {display: flex; flex-wrap: wrap;}", 0);
+    ss.insertRule(".agrid {display: block; }", 0);
+    //ss.insertRule(".card {margin: 6px}", 0);
+    ss.insertRule(".card {display: inline-block; margin: 6px 4px}", 0);
+    window.addEventListener("resize", calccardsize, false);
+})();
